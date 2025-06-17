d2729920ec4bf2c01b659cd1709eb1f883df2386
DDINGJOO <ddingsha9@teambind.co.kr>
2025-06-18 00:02:16 +0900

    [REPACK](jwt-token) : application 에서 변수 설정
    Signed-off-by: DDINGJOO <ddingsha9@teambind.co.kr>

--- backend/public/jwt-token/src/main/java/token/JwtTokenProvider.java
+++ backend/public/jwt-token/src/main/java/token/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -14,7 +15,11 @@ import java.util.Map;
 @Slf4j
 public class JwtTokenProvider {
 
+
+    @Value("${jwt.secret}")
     private final SecretKey key;
+
+    @Value("${jwt.expiration}")
     private final long expirationSeconds;
 
     public JwtTokenProvider(String secret, long expirationSeconds) {
