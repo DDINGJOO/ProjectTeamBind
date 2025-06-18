4a614c7236a6e5ccd3d3c0ba05d9e38823b78e7b
DDINGJOO <ddingsha9@teambind.co.kr>
2025-06-18 21:31:59 +0900

    [TEST](auth): AuthServiceTest
    Signed-off-by: DDINGJOO <ddingsha9@teambind.co.kr>

--- backend/service/auth/src/main/java/auth/dto/request/SignUpRequest.java
+++ backend/service/auth/src/main/java/auth/dto/request/SignUpRequest.java
@@ -6,10 +6,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
-
 public class SignUpRequest {
 
     String email;
     String password;
     String passwordConfirm;
+
+    public SignUpRequest(String mail, String s) {
+        this.email = mail;
+        this.password = s;
+
+    }
+
+    public SignUpRequest(String mail, String s, String differentPass)
+    {
+        this.email = mail;
+        this.password = s;
+        this.passwordConfirm = differentPass;
+
+    }
 }
