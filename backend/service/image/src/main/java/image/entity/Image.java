7db33200078108df1c013fde5c0e74494103d5ac
DDINGJOO <ddingsha9@teambind.co.kr>
2025-06-21 15:18:08 +0900

    [FEAT](image): 이미지 엔티티 설계 및 상태 기반 필드 추가 - 상태 필드 ImageStatus 도입 (TEMP, CONFIRMED, ...) - uploaderId, referenceId로 연관 정보 추적 가능하게 구성
    Signed-off-by: DDINGJOO <ddingsha9@teambind.co.kr>

---
+++ backend/service/image/src/main/java/image/entity/Image.java
@@ -0,0 +1,85 @@
+package image.entity;
+
+
+import image.config.eurm.ImageStatus;
+import image.config.eurm.ImageVisibility;
+import image.config.eurm.ResourceCategory;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "image_files",
+        indexes = {
+                @Index(name = "idx_image_file_category", columnList = "category"),
+                @Index(name = "idx_image_file_reference_id", columnList = "referenceId"),
+                @Index(name = "idx_image_file_category_and_referenceId", columnList = "category, referenceId"),
+                @Index(name = "idx_image_file_status", columnList = "status"),
+        }
+)
+@Getter
+@Builder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+
+    @Id
+    private Long id;
+
+    // 파일 이름 (uuid.jpg)
+    private String uuidName;
+
+    // 원본 파일 이름
+    private String originalName;
+
+    // 저장된 전체 경로 (예: /upload/images/POST/2025/05/28/uuid.jpg)
+    private String storedPath;
+
+    // 썸네일 경로 (선택)
+    private String url;
+
+    boolean isThumbnail;
+
+    // MIME 타입 (image/jpeg 등)
+    private String contentType;
+
+    // 파일 크기 (바이트 단위)
+    private Long fileSize;
+
+    // 카테고리 (PROFILE, POST, BAND_ROOM, BUSINESS_DOC 등)
+    @Enumerated(EnumType.STRING)
+    private ResourceCategory category;
+
+    // 참조 ID (ex. 게시글 ID, 유저 ID 등)
+    private String referenceId;
+
+    // 이미지 업로더 (누가 업로드했는지)
+    private String uploaderId;
+
+    // 이미지 상태 (TEMP, CONFIRMED, PENDING_DELETE)
+    @Enumerated(EnumType.STRING)
+    private ImageStatus status;
+
+    // 공개 여부 (PUBLIC or PRIVATE)
+    @Enumerated(EnumType.STRING)
+    private ImageVisibility visibility;
+
+    // 생성 시간
+    private LocalDateTime createdAt;
+
+    // 삭제 예정 시간 (PENDING_DELETE인 경우)
+    private LocalDateTime pendingDeleteAt;
+
+
+    //== 상태 전환 메서드 ==//
+    public void markPendingDelete() {
+        this.status = ImageStatus.PENDING_DELETE;
+        this.pendingDeleteAt = LocalDateTime.now();
+    }
+
+    public void confirm() {
+        this.status = ImageStatus.CONFIRMED;
+    }
+}
